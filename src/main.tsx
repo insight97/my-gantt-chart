c5353afa4992e6980bd79df22c9b5a6dbbe5c1db
insight97 <insight97gg@gmail.com>
2026-07-27 13:32:24 +0800

    integrate capacity summary into gantt

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,2 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import './styles.css';import App from './App';
+import React from 'react';import ReactDOM from 'react-dom/client';import './styles.css';import './capacity-header.css';import App from './App';
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
